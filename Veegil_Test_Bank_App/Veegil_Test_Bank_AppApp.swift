6a4e50bf5e75601be4b5a71d67410704626b16aa
Travis-ugo <okonicha.ugo@gmail.com>
2023-11-30 23:23:43 +0100

    Initial Commit

---
+++ Veegil_Test_Bank_App/Veegil_Test_Bank_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Veegil_Test_Bank_AppApp.swift
+//  Veegil_Test_Bank_App
+//
+//  Created by Travis Okonicha on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Veegil_Test_Bank_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
